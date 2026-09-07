787bde2f1439bf725f22789e265faa803e371555
Leonid <leo.sunny.home@gmail.com>
2026-09-07 21:45:02 +0300

    Update message in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-int main(){
-    std::cout << "Hello,I'm Leonid from group 13/4!" <<std::endl;
-    return 0;
-    
-}
+int main() {
+    std::cout << "Updated message!" << std::endl;
+     return 0;
+    }
